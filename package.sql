dcede1ef0a93da8e11a8e23afc8a0f372a8620ea
EasonDY <dreamy9dy@gmail.com>
2018-03-26 21:01:38 -0700

    Update package.sql

--- package.sql
+++ package.sql
@@ -14,11 +14,11 @@ employeeID 	CHAR(30),
 starttime	CHAR(30) not null,
 endtime	CHAR(30) not null,
 instance	CHAR(50) not null,
-PRIMARY KEY (customerID, employeeID),
-FOREIGN KEY (customerID) REFERENCES Customer
+primary key (customerID, employeeID),
+foreign key (customerID) REFERENCES Customer
 ON DELETE 
 ON UPDATE CASCADE
-FOREIGN KEY (employeeID) REFERENCES CustomerService
+foreign key (employeeID) REFERENCES CustomerService
 ON DELETE 
 ON UPDATE CASCADE
 };
@@ -28,8 +28,8 @@ employeeID 	CHAR(30),
 companyID	CHAR(30) not null,
 name		CHAR(30) not null,
 language	CHAR(30) not null,
-PRIMARY KEY (employeeID)
-FOREIGN KEY (companyID) REFERENCES DeliveryCompany
+primary key (employeeID)
+foreign key (companyID) REFERENCES DeliveryCompany
 ON DELETE
 ON UPDATE CASCADE
 };
@@ -38,21 +38,21 @@ create table DeliveryType{
 typename	CHAR(30),
 rate		DOUBLE not null,
 deliveryTime	CHAR(30) not null,
-PRIMARY KEY (typename)
+foreign key (typename)
 };
 
 create table DeliveryCompany{
 companyID	CHAR(30),
 cname		CHAR(30) not null,
-PRIMARY KEY (companyID)
+foreign key (companyID)
 };
 
 create table DeliveryCompanyAddress{
 companyID	CHAR(30),
 branch#	INT not null,
 caddress	CHAR(50) not null,
-PRIMARY KEY (companyID, branch#)
-FOREIGN KEY (companyID) REFERENCES DeliveryCompany
+primary key (companyID, branch#)
+foreign key (companyID) REFERENCES DeliveryCompany
 };
 
 create table CreateOrder{
@@ -67,15 +67,52 @@ receiverAddress	CHAR(50) not null,
 price			DOUBLE not null,
 dateCreated		DATE not null,
 expectedArrival	DATE not null,	
-PRIMARY KEY (orderID, customerID, companyID, typename)
-FOREIGN KEY (companyID) REFERENCES DeliveryCompany
+primary key (orderID, customerID, companyID, typename)
+foreign key (companyID) REFERENCES DeliveryCompany
 ON DELETE
 ON UPDATE CASCADE
-FOREIGN KEY (customerID) REFERENCES Customer
+foreign key (customerID) REFERENCES Customer
 ON DELETE
 ON UPDATE CASCADE
-FOREIGN KEY (typename) REFERENCES DeliveryType
+foreign key (typename) REFERENCES DeliveryType
 ON DELETE
+ON UPDATE CASCADE
+};
+
+create table UpdateExistingOrders{
+orderID	 	INT,
+currentLocation	CHAR(50) not null,
+status			CHAR(50),
+companyID		INT not null,
+dateupdated		DATE not null,
+instance		CHAR(50),
+primary key (orderID)
+foreign key (orderID) REFERENCES CreateOrder
+ON DELETE CASCADE
+ON UPDATE CASCADE
+foreign key (companyID) REFERENCES DeliveryCompany
+ON DELETE CASCADE
+ON UPDATE CASCADE
+};
+
+create table FinishedOrders{
+orderID	 	INT,
+finishedDate DATE not null,
+status CHAR(50),
+primary key (orderID)
+foreign key (orderID) REFERENCES CreateOrder
+ON DELETE CASCADE
+ON UPDATE CASCADE
+};
+
+create table PackageContained{
+orderID	INT,
+package#	INT,
+description CHAR(100),
+weight DOUBLE not null,
+PRIMARY KEY (orderID, package#)
+FOREIGN KEY (orderID) REFERENCES CreateOrder
+ON DELETE CASCADE
 ON UPDATE CASCADE
 };
 
