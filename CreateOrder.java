5e8e05e82455d0dcde718ea97795c7226de3c892
DoubleO31 <33016277+DoubleO31@users.noreply.github.com>
2018-03-29 23:28:15 -0700

    deleted oracle login info

--- CreateOrder.java
+++ CreateOrder.java
@@ -25,7 +25,7 @@ public class CreateOrder {
 
         try
         {
-            con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug","ora_a6o0b","a41533143");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug","","");
 
             System.out.println("\nConnected to Oracle!");
         }
